fe079cf0b4f064972b1d1ed1a793d681f699ba38
adamsylla97 <209474@edu.p.lodz.pl>
2019-06-02 20:52:00 +0200

    *test 1 green

---
+++ src/main/java/edu/iis/mto/serverloadbalancer/ServerBuilder.java
@@ -0,0 +1,15 @@
+package edu.iis.mto.serverloadbalancer;
+
+public class ServerBuilder {
+
+    private int capacity;
+
+    public static Server build() {
+        return new Server();
+    }
+
+    public ServerBuilder withCapacity(int capacity) {
+        this.capacity = capacity;
+        return this;
+    }
+}
