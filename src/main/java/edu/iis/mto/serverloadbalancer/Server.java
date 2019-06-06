fde19e5ed6834be5da3d2b1f1a11880dd43f85a0
adamsylla97 <209474@edu.p.lodz.pl>
2019-06-06 14:46:36 +0200

    *refactor

--- src/main/java/edu/iis/mto/serverloadbalancer/Server.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -4,6 +4,7 @@ public class Server {
 
     public double currentLoadPercentage;
     public int capacity;
+    private final double MAXIMUM_LOAD = 100.0d;
 
     public boolean contains(Vm theVm) {
         return true;
@@ -11,5 +12,9 @@ public class Server {
 
     public Server(int capacity) {
         this.capacity = capacity;
+    }
+
+    public void addVm(Vm vm) {
+        currentLoadPercentage = (double)vm.size / capacity * MAXIMUM_LOAD;
     }
 }
