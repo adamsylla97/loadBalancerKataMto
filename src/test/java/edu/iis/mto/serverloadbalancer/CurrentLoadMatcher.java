37336fd77e833805d99bb25a657ec20199538079
adamsylla97 <209474@edu.p.lodz.pl>
2019-06-06 14:26:48 +0200

    *test 1 green

---
+++ src/test/java/edu/iis/mto/serverloadbalancer/CurrentLoadMatcher.java
@@ -0,0 +1,23 @@
+package edu.iis.mto.serverloadbalancer;
+
+import org.hamcrest.Description;
+import org.hamcrest.TypeSafeMatcher;
+
+public class CurrentLoadMatcher extends TypeSafeMatcher<Server> {
+
+    private double expectedLoadPercentage;
+
+    public CurrentLoadMatcher(double expectedLoadPercentage) {
+
+        this.expectedLoadPercentage = expectedLoadPercentage;
+    }
+
+    @Override protected boolean matchesSafely(Server server) {
+        return expectedLoadPercentage == server.currentLoadPercentage ||
+               Math.abs(expectedLoadPercentage - server.currentLoadPercentage) < 0.01d;
+    }
+
+    @Override public void describeTo(Description description) {
+        description.appendText("a server with load percentage of ").appendValue(expectedLoadPercentage);
+    }
+}
