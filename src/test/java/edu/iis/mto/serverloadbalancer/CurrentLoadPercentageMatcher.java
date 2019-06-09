500ce8111405ad245e28db944a27f50f304b129a
adamsylla97 <209474@edu.p.lodz.pl>
2019-06-09 20:04:22 +0200

    *refactor

--- src/test/java/edu/iis/mto/serverloadbalancer/CurrentLoadPercentageMatcher.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/CurrentLoadPercentageMatcher.java
@@ -7,17 +7,25 @@ import org.hamcrest.TypeSafeMatcher;
 public class CurrentLoadPercentageMatcher extends TypeSafeMatcher<Server> {
 
     private double expectedServerLoadPercentage;
+    private static final double EPSILON = 0.01d;
 
     public CurrentLoadPercentageMatcher(double expectedServerLoadPercentage) {
         this.expectedServerLoadPercentage = expectedServerLoadPercentage;
     }
 
     @Override protected boolean matchesSafely(Server server) {
-        return expectedServerLoadPercentage == server.currentLoadPercentage ||
-               Math.abs(expectedServerLoadPercentage - server.currentLoadPercentage) < 0.01d;
+        return doublesAreEqual(expectedServerLoadPercentage, server.currentLoadPercentage);
+    }
+
+    private boolean doublesAreEqual(double d1, double d2){
+        return d1 == d2 || Math.abs(d1 - d2) < EPSILON;
     }
 
     @Override public void describeTo(Description description) {
         description.appendText("current server load percentage: ").appendValue(expectedServerLoadPercentage);
+    }
+
+    public static CurrentLoadPercentageMatcher hasCurrentLoadPercentage(double expectedServerLoadPercentage) {
+        return new CurrentLoadPercentageMatcher(expectedServerLoadPercentage);
     }
 }
