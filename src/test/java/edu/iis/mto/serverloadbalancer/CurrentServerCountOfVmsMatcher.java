72ee8efeff5362eba16f59abd075f57a0f6169f6
Adam Sylla <sylla.adam.97@gmail.com>
2019-06-10 14:50:12 +0200

    *test 4 green

---
+++ src/test/java/edu/iis/mto/serverloadbalancer/CurrentServerCountOfVmsMatcher.java
@@ -0,0 +1,29 @@
+package edu.iis.mto.serverloadbalancer;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+
+public class CurrentServerCountOfVmsMatcher extends TypeSafeMatcher<Server> {
+
+    private int expectedVmsCount;
+
+    public CurrentServerCountOfVmsMatcher(int expectedVmsCount) {
+        this.expectedVmsCount = expectedVmsCount;
+    }
+
+    @Override
+    protected boolean matchesSafely(Server server) {
+        return expectedVmsCount == server.getVmsCount();
+    }
+
+    @Override
+    protected void describeMismatchSafely(Server item, Description description) {
+        description.appendText("vms count that server actually contains: ").appendValue(item.getVmsCount());
+    }
+
+    @Override
+    public void describeTo(Description description) {
+        description.appendText("vms count that server actually contains: ").appendValue(expectedVmsCount);
+    }
+}
