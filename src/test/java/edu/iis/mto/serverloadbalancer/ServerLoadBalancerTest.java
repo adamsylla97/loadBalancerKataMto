3a2b8b67ccbf04d72ec87c39c4b3896b2179d325
adamsylla97 <209474@edu.p.lodz.pl>
2019-06-10 18:52:08 +0200

    *test 2 refactor

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -3,11 +3,13 @@ package edu.iis.mto.serverloadbalancer;
 
 import static edu.iis.mto.serverloadbalancer.CurrentServerLoadMatcher.*;
 import static edu.iis.mto.serverloadbalancer.ServerBuilder.*;
+import static edu.iis.mto.serverloadbalancer.VmBuilder.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 import org.hamcrest.Matcher;
 import org.junit.Test;
+import org.junit.rules.Timeout;
 
 public class ServerLoadBalancerTest {
 
@@ -42,10 +44,6 @@ public class ServerLoadBalancerTest {
 		return vms;
 	}
 
-	private VmBuilder vm() {
-		return new VmBuilder();
-	}
-
 	private void balancing(Server[] servers, Vm[] vms) {
 		new ServerLoadBanacer().balance(servers, vms);
 	}
@@ -58,11 +56,7 @@ public class ServerLoadBalancerTest {
 		return servers;
 	}
 
-	private Server a(ServerBuilder builder) {
-		return builder.build();
-	}
-
-	private Vm a(VmBuilder builder){
+	private <T> T a(Builder<T> builder){
 		return builder.build();
 	}
 
