2f9e2de8227443a12ac5e12e56d977dbbdcaee97
adamsylla97 <209474@edu.p.lodz.pl>
2019-06-11 09:52:13 +0200

    *test 2 green

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -25,6 +25,27 @@ public class ServerLoadBalancerTest {
 
 	}
 
+	@Test
+	public void balancingOneServerWithOneSlotCapacity_andOneSlotVm_fillsTheServerWithTheVm(){
+
+		Server theServer = a(server().withCapacity(1));
+		Vm theVm = a(vm().ofSize(1));
+
+		balancing(listWithServers(theServer), listWithVms(theVm));
+
+		assertThat(theServer, hasCurrentLoadPercentageOf(100.0d));
+		assertThat("server should contain the vm", theServer.contains(theVm));
+
+	}
+
+	private Vm[] listWithVms(Vm... vms) {
+		return vms;
+	}
+
+	private VmBuilder vm() {
+		return new VmBuilder();
+	}
+
 	private void balancing(Server[] servers, Vm[] vms) {
 		new ServerLoadBalancer().balance(servers,vms);
 	}
@@ -38,6 +59,10 @@ public class ServerLoadBalancerTest {
 	}
 
 	private Server a(ServerBuilder builder) {
+		return builder.build();
+	}
+
+	private Vm a(VmBuilder builder){
 		return builder.build();
 	}
 
